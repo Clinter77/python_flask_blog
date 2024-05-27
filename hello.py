09d4b83ea261d73b63bd616cb854cebe4637ebfb
clinter77 <clintchris7777@laposte.net>
2024-05-27 14:01:38 +0200

    commit du 27 mai 2024 à 14h

--- hello.py
+++ hello.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     # return 'Hello World !'
-    return '<h3 style="color:blue;">Welcome on my Web site :)</h3>'
+    return '<h3 style="color:blue; text-align:center; margin:auto; margin-top:20vh;font-size:3em;">Welcome on my Web site :)</h3>'
 
-
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug=True)
